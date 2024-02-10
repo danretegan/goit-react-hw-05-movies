1a24ba791e16a8cc11a686e611ac5a1eb2ae66cc
Dan <danretegan@yahoo.com>
2024-02-10 19:07:34 +0200

    Reviews.jsx setup

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,5 +1,44 @@
-function Reviews() {
-  return <div>Reviews...</div>;
-}
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovieReviews } from '../services/api';
+
+const Reviews = () => {
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const movieReviews = async () => {
+      try {
+        const response = await fetchMovieReviews(movieId);
+        setReviews(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    movieReviews();
+  }, [movieId]);
+
+  return (
+    <>
+      {reviews.length !== 0 && (
+        <div>
+          <h2>Movie Reviews</h2>
+          <ul>
+            {reviews.map(review => (
+              <li key={review.id}>
+                <p>{review.author}</p>
+                <p>{review.content}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {reviews.length === 0 && (
+        <div>We don't have any reviews for this movie.</div>
+      )}
+    </>
+  );
+};
 
 export default Reviews;
