1a7b766263a1921054afc0d75a6082b4ae558bba
Dan <danretegan@yahoo.com>
2024-02-10 12:51:31 +0200

    add initial structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Movies from '../pages/Movies';
+import MovieDetails from '../pages/MovieDetails';
+import Cast from '../pages/Cast';
+import Reviews from '../pages/Reviews';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/movies/:movieId" element={<MovieDetails />} />
+      <Route path="/movies/:movieId/cast" element={<Cast />} />
+      <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+    </Routes>
   );
 };
+
+export default App;
