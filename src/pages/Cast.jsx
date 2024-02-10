355573f4da11395605af9500a06807e8ccc26d97
Dan <danretegan@yahoo.com>
2024-02-10 18:45:41 +0200

    Cast.jsx setup

--- src/pages/Cast.jsx
+++ src/pages/Cast.jsx
@@ -1,5 +1,49 @@
-function Cast() {
-  return <div>Cast...</div>;
-}
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovieCast } from '../services/api';
+
+const Cast = () => {
+  const { movieId } = useParams();
+  const [cast, setCast] = useState([]);
+
+  useEffect(() => {
+    const movieCast = async () => {
+      try {
+        const response = await fetchMovieCast(movieId);
+        setCast(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    movieCast();
+  }, [movieId]);
+
+  return (
+    <>
+      {cast.length !== 0 && (
+        <div>
+          <h2>Movie Cast</h2>
+          {cast.map(actor => (
+            <div key={actor.id}>
+              <img
+                width="200px"
+                height="300px"
+                src={
+                  actor.profile_path
+                    ? `https://image.tmdb.org/t/p/w300${actor.profile_path}`
+                    : `https://i.imgur.com/p3MsT9t.jpg`
+                }
+                alt={actor.original_name}
+              />
+              <p>{actor.name}</p>
+            </div>
+          ))}
+        </div>
+      )}
+      {cast.length === 0 && <div>We don't have any cast for this movie.</div>}
+    </>
+  );
+};
 
 export default Cast;
