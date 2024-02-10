29e10682f45d2ae28d6be9ff5ed9cd30f07b3d4d
Dan <danretegan@yahoo.com>
2024-02-10 16:35:42 +0200

    Home.jsx setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,35 @@
-function Home() {
-  return <div>Home...</div>;
-}
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { fetchTrendingMovies } from '../services/api';
+
+const Home = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const movies = await fetchTrendingMovies();
+        setTrendingMovies(movies);
+      } catch (error) {
+        console.error('Error fetching trending movies:', error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <h2>Trending today</h2>
+      <ul>
+        {trendingMovies.map(movie => (
+          <li key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Home;
