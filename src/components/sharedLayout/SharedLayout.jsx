5f4070737c7de46a1f843d2d57ddde1bb83fad39
Dan <danretegan@yahoo.com>
2024-02-11 10:31:01 +0200

    SharedLayout stilization update

--- src/components/sharedLayout/SharedLayout.jsx
+++ src/components/sharedLayout/SharedLayout.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 import styles from './SharedLayout.module.css';
 
 const SharedLayout = () => {
+  const location = useLocation();
+
+  const isMoviesPage = location.pathname === '/movies';
+  const isHomePage = location.pathname === '/';
+
   return (
     <>
       <nav className={styles.navbar}>
-        <Link to="/" className={styles.navLink}>
+        <Link
+          to="/"
+          className={`${styles.navLink} ${isHomePage ? styles.homePage : ''}`}
+        >
           Home
         </Link>
-        <Link to="/movies" className={styles.navLink}>
+        <Link
+          to="/movies"
+          className={`${styles.navLink} ${
+            isMoviesPage ? styles.moviesPage : ''
+          }`}
+        >
           Movies
         </Link>
       </nav>
