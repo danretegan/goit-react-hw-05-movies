1618eca08850eb81dcff2d3ceb07622015d03b8c
Dan <danretegan@yahoo.com>
2024-02-20 14:52:36 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
 import React, { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-
-import Home from '../pages/Home';
-import Movies from '../pages/Movies';
-import MovieDetails from '../pages/movieDetails/MovieDetails';
 import SharedLayout from './sharedLayout/SharedLayout';
-import NotFoundPage from '../pages/NotFoundPage';
+import Home from '../pages/Home';
 
 //  Importurile dinamice (lazy()) trebuie facute DUPA importurile statice!
 
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/movieDetails/MovieDetails'));
 const Cast = lazy(() => import('../pages/cast/Cast'));
 const Reviews = lazy(() => import('../pages/Reviews'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
 
 const App = () => {
   return (
